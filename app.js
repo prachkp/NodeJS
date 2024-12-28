8ed2ddeab2b4bb30b21597f972abda234c575c54
Prach Konphet <prach_kp@outlook.com>
2024-12-28 21:27:09 +0700

    rename to index.html

--- app.js
+++ app.js
@@ -8,20 +8,28 @@ const app = express();
 // const port = process.env.PORT;
 const port = process.env.port;
 
+const productRouter = express.Router();
+
 // app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+productRouter.route("/").get((req, res) => {
+    res.send("Hello World !! I'm products");
+});
+
+app.use("/products", productRouter);
+
 // app.use("products", "../../public/products");
 // app.use("products", "../../public/signin.html");
 
-app.get("/", (req,res) =>{
-    res.render("index", {username: 'Prach Konphet', customers: ["Prawit", "Presan", "Prawat"]});
+app.get("/", (req, res) => {
+    res.render("index", { username: 'Prach Konphet', customers: ["Prawit", "Presan", "Prawat"] });
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     // debug("Listening on port" + chalk.green(" : "+port));
-    console.log("Listening on port :" +port);
+    console.log("Listening on port :" + port);
 })
